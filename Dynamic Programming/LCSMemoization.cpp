3f1f3c8e1edf314acd4e0ece728dfd0d9c69ac0a
Anmol Saraswat <anmolsaraswat@outlook.com>
2021-05-04 18:42:21 +0530

    Longest Common Subsequence using Memoization

---
+++ Dynamic Programming/LCSMemoization.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[1000][1000];
+
+
+int lcs (char *X, char *Y, int m, int n)
+{
+    if(X[m] == '\0' || Y[n] == '\0')
+        return 0;
+    if(arr[m][n] != -1)
+        return arr[m][n];
+    if(X[m] == Y[n])
+    {
+        arr[m][n] = 1 + lcs(X, Y, m+1, n+1);
+        return arr[m][n];
+    }
+    else
+    {
+        arr[m][n] = max(lcs(X, Y, m, n+1), lcs(X, Y, m+1, n));
+        return arr[m][n];
+    }
+
+}
+
+int main()
+{
+    memset(arr, -1, sizeof(arr));
+    char X[] = "AGGTAB";
+    char Y[] = "GXTXAYB";
+
+    cout<< "Length of LCS is " << lcs(X, Y, 0, 0);
+
+    return 0;
+}
